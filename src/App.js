cbb89a3e9e947a7a83c8b0ffe2b8966d3ac065c9
ANUBHAV KUMAR SAH <139559243+Anubhav-Sah@users.noreply.github.com>
2025-03-10 00:16:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
+  const location = useLocation(); // Get the current route
+
+  // Check if the current page is Login or Signup
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
+
   return (
-    <div>
-      <Home />
-      <Navbar /> 
-    </div>
+    <>
+       {/* Always show Navbar */}
+
+      
+      {!isAuthPage && <Home />}
+      {!isAuthPage &&<Navbar />/* Show Home only if NOT on Login or Signup */}
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
