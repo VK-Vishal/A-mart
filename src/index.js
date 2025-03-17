96f7b0e1ca0573e040f035c8e0f5842f2831dd74
Vishal <vadrangi.vishalkumar@gmail.com>
2025-03-17 23:12:56 +0530

    hosting

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/A-mart"> {/* Add basename for GitHub Pages */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
